3c6d07f899572df8031b791cb4bb52a7bd365ba8
Gram <master_fess@mail.ru>
2019-03-20 17:16:52 +0300

    tests

---
+++ tests/test_getters.py
@@ -0,0 +1,20 @@
+# external
+import pytest
+from packaging.version import Version
+
+# project
+from dephell.pythons import Pythons
+
+
+@pytest.mark.parametrize('version', [
+    '2.7',
+    '3.0',
+    '3.4',
+    '3.5',
+    '3.6',
+    '3.7',
+])
+def test_get_by_version(version):
+    manager = Pythons(abstract=True)
+    python = manager.get_by_version(Version(version))
+    assert str(python.version).startswith(version)
