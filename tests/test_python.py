cd1cf1aa33b4ca65c833c298b81bfb02650b4102
Gram <master_fess@mail.ru>
2019-04-06 16:39:28 +0300

    fix libs test for win

--- tests/test_python.py
+++ tests/test_python.py
@@ -5,7 +5,10 @@ from dephell_pythons import Pythons
 def test_lib_paths():
     p = Pythons().current
     assert len(p.lib_paths) > 2
-    assert set(map(str, p.lib_paths)) == set(sys.path)
+    # appveyor makes some hacks for python path
+    libs_found = {path for path in sys.path if 'Scripts' not in path and 'dephell' not in path}
+    libs_real = {str(path) for path in p.lib_paths if 'Scripts' not in str(path) and 'dephell' not in str(path)}
+    assert libs_found == libs_real
 
 
 def test_lib_path():
