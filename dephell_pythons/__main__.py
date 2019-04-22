93c1aacdea2dfe5e155d0c413e74ebcf4076f8ec
Gram <master_fess@mail.ru>
2019-04-22 19:23:48 +0300

    get versions from pyenv not from shims

--- dephell_pythons/__main__.py
+++ dephell_pythons/__main__.py
@@ -5,9 +5,10 @@ from ._finder import Finder
 
 finder = Finder()
 pythons = []
-for python in finder.get_pythons():
+for path in finder.get_pythons():
     pythons.append(dict(
-        path=str(python),
-        version=finder.get_version(python),
+        path=str(path),
+        version=finder.get_version(path),
+        shim=finder.in_shims(path=path),
     ))
 print(json.dumps(pythons, sort_keys=True, indent=2))
--- tests/test_abstract.py
+++ tests/test_abstract.py
@@ -6,6 +6,5 @@ def test_abstract():
     manager = Pythons(abstract=True)
     pythons = {'.'.join(map(str, p.version.release[:2])): p for p in manager}
     assert len({'2.7', '3.5', '3.7'} - set(pythons)) == 0
-    assert pythons['3.8'].abstract is True
     assert pythons['4.0'].abstract is True
     assert not all(p.abstract for p in manager)
