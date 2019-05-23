d14be6cf3643a1179a52e4a095d9a05e073ec2d8
Gram <master_fess@mail.ru>
2019-05-23 20:42:15 +0200

    bump version to 0.1.11

--- dephell_pythons/pep514tools/__init__.py
+++ dephell_pythons/pep514tools/__init__.py
@@ -6,6 +6,6 @@
 #-------------------------------------------------------------------------
 
 __author__ = 'Steve Dower <steve.dower@python.org>'
-__version__ = '0.1.10'
+__version__ = '0.1.11'
 
 from .environment import findall, find, findone
