750374dee039eddf528219469a7061264561af0f
Gram <master_fess@mail.ru>
2019-07-24 15:55:39 +0200

    check read access for dir

--- dephell_pythons/_shell_utils.py
+++ dephell_pythons/_shell_utils.py
@@ -5,6 +5,11 @@ from pathlib import Path
 
 # https://stackoverflow.com/a/377028/8704691
 def is_file(path: Path) -> bool:
+    """
+    1. It exists
+    2. It's a file
+    3. We can execute it
+    """
     try:
         if not path.is_file():
             return False
@@ -16,11 +21,16 @@ def is_file(path: Path) -> bool:
 
 
 def is_dir(path: Path) -> bool:
+    """
+    1. It exists
+    2. It's a dir
+    3. We can read it
+    """
     try:
         if not path.is_dir():
             return False
     except PermissionError:
         return False
-    if not os.access(str(path), os.X_OK):
+    if not os.access(str(path), os.R_OK):
         return False
     return True
